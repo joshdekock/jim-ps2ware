cf1a5605b6284de18b14b60cb9e0de8946e683fb
nobody <nobody@5c23000f-aefb-0310-8550-8024f1b2be0e>
2004-08-15 19:03:03 +0000

    This commit was manufactured by cvs2svn to create branch 'GNU'.

--- wxVU/src/parser.h
+++
@@ -1,5 +0,0 @@
-void dlower(uint32 *lower, char *low, char *lparam);
-void dupper(uint32 *upper, char *upp, char *uparam);
-int insert(char *upper, char *lower, char *uparam, char *lparam, uint32 index);
-int LoadCode(char *file);
-int LoadInstructions(char *file);
